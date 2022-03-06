19fbff26039962e8b2443c0653deaa4ec8e271bf
wu2ee <gulb1602@gmail.com>
2022-03-06 10:42:33 +0900

    [java] inflearn problem03

---
+++ Algorithm(Java)/algorithm/src/main/java/inflearn/Problem03.java
@@ -0,0 +1,23 @@
+package inflearn;
+
+import java.util.Scanner;
+
+public class Problem03 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] words = scanner.nextLine().split(" ");
+        int max_length = -1;
+        for (String word : words) {
+            if (word.length() > max_length) {
+                max_length = word.length();
+            }
+        }
+        for (String word : words) {
+            if (word.length() == max_length) {
+                System.out.println(word);
+                return ;
+            }
+        }
+    }
+
+}
