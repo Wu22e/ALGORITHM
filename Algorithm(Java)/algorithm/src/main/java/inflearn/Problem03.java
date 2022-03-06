4d53a8e789fc184ed35bcbfc615ba03fda49e0c4
wu2ee <gulb1602@gmail.com>
2022-03-06 22:34:16 +0900

    [java] inflearn problem03
    - Integer.MIN_VALUE 이용하기

--- Algorithm(Java)/algorithm/src/main/java/inflearn/Problem03.java
+++ Algorithm(Java)/algorithm/src/main/java/inflearn/Problem03.java
@@ -6,7 +6,7 @@ public class Problem03 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String[] words = scanner.nextLine().split(" ");
-        int max_length = -1;
+        int max_length = Integer.MIN_VALUE;
         for (String word : words) {
             if (word.length() > max_length) {
                 max_length = word.length();
