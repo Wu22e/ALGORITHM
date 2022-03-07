0f96a2c777b821de6f382b63ef7837b634e8ead9
wu2ee <gulb1602@gmail.com>
2022-03-08 02:24:48 +0900

    [java] inflearn problem05_1, problem05_2

---
+++ Algorithm(Java)/algorithm/src/main/java/inflearn/Problem05_2.java
@@ -0,0 +1,28 @@
+package inflearn;
+
+import java.util.Scanner;
+
+public class Problem05_2 {
+    public static void main(String[] args) {
+        // 2. 이중포인터 이용
+        Scanner scanner = new Scanner(System.in);
+        char[] input = scanner.nextLine().toCharArray();
+        int lt = 0, rt = input.length - 1;
+        while (lt < rt) {
+            if (!Character.isAlphabetic(input[lt])) {
+                lt++;
+            } else if (!Character.isAlphabetic(input[rt])) {
+                rt--;
+            } else {
+                char temp = input[lt];
+                input[lt] = input[rt];
+                input[rt] = temp;
+                lt++;
+                rt--;
+            }
+        }
+
+        System.out.println(String.valueOf(input));
+
+    }
+}
