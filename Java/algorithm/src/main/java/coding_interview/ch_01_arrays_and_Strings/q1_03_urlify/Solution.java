9585321c5d42642ba086c3e77d07257da2bffbd2
Wu22e <gulb1602@gmail.com>
2023-09-15 23:34:51 +0900

    [java | coding-interview] ch1_q3 urlify

---
+++ Java/algorithm/src/main/java/coding_interview/ch_01_arrays_and_Strings/q1_03_urlify/Solution.java
@@ -0,0 +1,18 @@
+package coding_interview.ch_01_arrays_and_Strings.q1_03_urlify;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class Solution {
+    private static final String blank = "%20";
+
+    public static void main(String[] args) {
+        String s = "Mr John Smith    ";
+        System.out.println(makeURL(s));
+    }
+
+    static public String makeURL(String s) {
+        String[] splitString = s.split(" ");
+        return Arrays.stream(splitString).collect(Collectors.joining(blank));
+    }
+}
